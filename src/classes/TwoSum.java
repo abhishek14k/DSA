57391badbafb9ff6815b79c92d4e8cf3e7d55906
Abhishek R <37078073+abhishek14k@users.noreply.github.com>
2023-02-09 09:27:02 +0530

    Commit two sum o(n)

--- src/classes/TwoSum.java
+++ src/classes/TwoSum.java
@@ -1,6 +1,7 @@
 package classes;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
 
@@ -18,15 +19,38 @@ public class TwoSum {
         return new int[] {};
     }
 
+    //O(n) Time complexity
+    public static int[] twoSumOn(int[] nums, int target) {
+        int n = nums.length;
+        int[] res = new int[2];
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i=0; i<n; i++) {
+            int temp = target - nums[i];
+            if(hm.containsKey(temp)) {
+                res[0] = hm.get(temp);
+                res[1] = i;
+                return res;
+            }
+            hm.put(nums[i], i);
+        }
+        return res;
+    }
+
+
     public static void main(String[] args) {
 
         System.out.println("Two sum!!");
         int[] nums = {2,7,11,15};
         System.out.println("Input: "+Arrays.toString(nums));
-        int target = 17;
+        int target = 18;
         System.out.println("Target: "+target);
 
-        int[] Result = twoSum(nums, target);
-        System.out.println("Result: "+Arrays.toString(Result));
+        int[] Result1 = twoSum(nums, target);
+        System.out.println("Result: "+Arrays.toString(Result1));
+
+        target = 17;
+        int[] Result2 = twoSumOn(nums, target);
+        System.out.println("Result: "+Arrays.toString(Result2));
     }
 }
